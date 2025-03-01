0e5aad9e3c243c83621dde3a3d9a901a038decb6
ejim11 <favourejim56@gmail.com>
2025-03-01 22:38:27 +0100

    ✅ Connected app to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,49 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import enviromentValidation from './config/enviroment.validation';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+/**
+ * app environment
+ */
+const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      load: [appConfig, databaseConfig],
+      validationSchema: enviromentValidation,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          autoLoadEntities: configService.get('database.autoLoadEntities'),
+          synchronize: configService.get('database.synchronize'),
+          port: +configService.get('database.port'),
+          username: configService.get('database.user'),
+          password: configService.get('database.password'),
+          host: configService.get('database.host'),
+          database: configService.get('database.name'),
+          ssl:
+            process.env.NODE_ENV === 'development'
+              ? false
+              : {
+                  rejectUnauthorized: false,
+                },
+          logging: process.env.NODE_ENV !== 'production',
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
