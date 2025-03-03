25bc51b0924221c72ab6836cdf82638cd48336ab
ejim11 <favourejim56@gmail.com>
2025-03-03 20:15:57 +0100

    👽️ removed api route for creating a user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from './dtos/create-user.dto';
-import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { AuthType } from 'src/auth/enums/auth-type.enum';
-import { Auth } from 'src/auth/decorator/auth.decorator';
+import { Controller } from '@nestjs/common';
+
 import { UsersService } from './providers/users.service';
 
 @Controller('users')
@@ -13,47 +10,4 @@ export class UsersController {
      */
     private readonly usersService: UsersService,
   ) {}
-
-  /**
-   * route for creating a user
-   * @param createUserDto
-   * @returns the created user
-   */
-  @Post('/')
-  @Auth(AuthType.None)
-  @ApiOperation({
-    summary: 'It creates a user ',
-  })
-  // documentation for responses
-  @ApiResponse({
-    status: 201,
-    description: 'User created successfully based on the query',
-  })
-  @ApiBody({
-    description: 'Contains user details',
-    required: true,
-    type: CreateUserDto,
-    examples: {
-      example1: {
-        summary: 'Valid request example',
-        value: {
-          fullname: 'Ejim Favour',
-          email: 'iloghaluagneskc@gmail.com',
-          role: 'ticketPurchaser',
-          password: '@Password1',
-        },
-      },
-      example2: {
-        summary: 'Invalid request example (missing password)',
-        value: {
-          fullname: 'Ejim Favour',
-          email: 'iloghaluagneskc@gmail.com',
-          role: 'ticketPurchaser',
-        },
-      },
-    },
-  })
-  public createUser(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.createUser(createUserDto);
-  }
 }
