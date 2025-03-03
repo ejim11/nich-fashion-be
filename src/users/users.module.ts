bf0bd6327a6aaf7cb52cb83d893d8e41f1e75436
ejim11 <favourejim56@gmail.com>
2025-03-03 20:16:22 +0100

    ✅ updated the correct names and imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,15 +2,15 @@ import { forwardRef, Module } from '@nestjs/common';
 import { UsersService } from './providers/users.service';
 import { UsersController } from './users.controller';
 import { AuthModule } from 'src/auth/auth.module';
-import { CreateUsersProvider } from './providers/create-users.providers';
+import { CreateUsersProvider } from './providers/create-users.provider';
 import { User } from './user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FindOneUserByEmailProvider } from './providers/find-one-user-by-email.provider';
 import { StoreOtpAndExpireProvider } from './providers/store-otp-and-expire.provider';
-import { FindUserByResetOtpAndExpiryTimeProvider } from './providers/find-user-by-reset-otp-and-expiry-time.provider';
-import { ChangeUserPasswordProvider } from './providers/change-user-password.provider';
+import { FindUserByOtpAndExpiryTimeProvider } from './providers/find-user-by-reset-otp-and-expiry-time.provider';
 import { FindOneByIdProvider } from './providers/find-one-by-id.provider';
 import { PaginationModule } from 'src/common/pagination/pagination.module';
+import { ClearOtpAndExpiryTimeProvider } from './providers/clear-otp-and-expiry-time.provider';
 
 @Module({
   providers: [
@@ -18,9 +18,9 @@ import { PaginationModule } from 'src/common/pagination/pagination.module';
     CreateUsersProvider,
     FindOneUserByEmailProvider,
     StoreOtpAndExpireProvider,
-    FindUserByResetOtpAndExpiryTimeProvider,
-    ChangeUserPasswordProvider,
+    FindUserByOtpAndExpiryTimeProvider,
     FindOneByIdProvider,
+    ClearOtpAndExpiryTimeProvider,
   ],
   controllers: [UsersController],
   imports: [
