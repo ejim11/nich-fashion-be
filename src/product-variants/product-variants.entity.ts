2581800fb7d34f64b73efc71bf409e21de000105
ejim11 <favourejim56@gmail.com>
2025-03-24 23:59:07 +0100

    ✅ created the product variant entity

---
+++ src/product-variants/product-variants.entity.ts
@@ -0,0 +1,35 @@
+import { ProductImage } from 'src/product-images/product-image.entity';
+import { Product } from 'src/products/product.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class ProductVariant {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  color: string;
+
+  @Column()
+  size: string;
+
+  @Column('int')
+  quantity: number;
+
+  @Column({ default: false })
+  soldOut: boolean;
+
+  @ManyToOne(() => Product, (product) => product.variants)
+  product: Product;
+
+  @OneToMany(() => ProductImage, (image) => image.productVariant, {
+    cascade: true,
+  })
+  images: ProductImage[];
+}
