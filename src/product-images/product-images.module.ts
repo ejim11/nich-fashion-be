48fbf5308c17614152c50a7b539401b8ce2cc5b9
ejim11 <favourejim56@gmail.com>
2025-03-25 23:53:05 +0100

    ✅ added the Product Image entity to the Product Images Module

--- src/product-images/product-images.module.ts
+++ src/product-images/product-images.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductImagesService } from './providers/product-images.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductImage } from './product-image.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProductImage])],
   providers: [ProductImagesService],
 })
 export class ProductImagesModule {}
