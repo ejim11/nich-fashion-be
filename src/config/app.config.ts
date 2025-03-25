0d5870995abf26aa9e5db1084a80a3307a0bba43
ejim11 <favourejim56@gmail.com>
2025-03-25 23:51:45 +0100

    ✅ app config aws

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -6,6 +6,11 @@ export default registerAs('appConfig', () => ({
   mailHost: process.env.MAIL_HOST,
   smtpUsername: process.env.SMTP_USERNAME,
   smtpPassword: process.env.SMTP_PASSWORD,
+  awsBucketName: process.env.AWS_PUBLIC_BUCKET_NAME,
+  awsRegion: process.env.AWS_REGION,
+  awsCloudFrontUrl: process.env.AWS_CLOUDFRONT_URL,
+  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
   host:
     process.env.NODE_ENV === 'development'
       ? process.env.LOCAL_HOST
