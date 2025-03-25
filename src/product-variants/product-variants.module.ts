9b8e8e1fcfaeb8036cb0665f7cfdc6981950e0f8
ejim11 <favourejim56@gmail.com>
2025-03-25 23:54:56 +0100

    ✅ added the Product Variant entity to Product Variant Products Variant Module

--- src/product-variants/product-variants.module.ts
+++ src/product-variants/product-variants.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductVariantsService } from './providers/product-variants.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductVariant } from './product-variants.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProductVariant])],
   providers: [ProductVariantsService],
 })
 export class ProductVariantsModule {}
