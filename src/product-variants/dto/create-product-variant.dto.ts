ba732ee06320d5e64924080f154a319419c986ad
ejim11 <favourejim56@gmail.com>
2025-03-25 23:54:06 +0100

    🏷️ added types to the create product variant dto

--- src/product-variants/dto/create-product-variant.dto.ts
+++ src/product-variants/dto/create-product-variant.dto.ts
@@ -1,7 +1,31 @@
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+
 export class CreateProductVariantDto {
+  @IsString()
+  @IsNotEmpty()
   color: string;
+
+  @IsString()
+  @IsNotEmpty()
   size: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  @Min(1)
   quantity: number;
 
-  images: string[];
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => String)
+  images?: string[];
 }
