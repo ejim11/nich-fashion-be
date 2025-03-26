4b3cec080c4278e8bcefb0666d0da6b7b11de0d2
ejim11 <favourejim56@gmail.com>
2025-03-26 23:59:25 +0100

    ✅ added the review entity to the review module

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReviewsController } from './reviews.controller';
 import { ReviewsService } from './providers/reviews.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Review } from './reviews.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Review])],
   controllers: [ReviewsController],
   providers: [ReviewsService],
 })
