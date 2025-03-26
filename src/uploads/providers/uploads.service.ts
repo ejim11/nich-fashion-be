502b6d3b5ae16d0efba5d95e482e263a2987fab5
ejim11 <favourejim56@gmail.com>
2025-03-27 00:00:28 +0100

    ✅ added the upload file functionality to the upload service

---
+++ src/uploads/providers/uploads.service.ts
@@ -0,0 +1,48 @@
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { UploadToAwsProvider } from './upload-to-aws.provider';
+
+@Injectable()
+export class UploadsService {
+  /**
+   * constructor
+   * @param uploadToAwsProvider
+   * @param configService
+   */
+  constructor(
+    /**
+     * injecting the upload to aws provider
+     */
+    private readonly uploadToAwsProvider: UploadToAwsProvider,
+
+    /**
+     * injecting config service
+     */
+    private readonly configService: ConfigService,
+  ) {}
+
+  /**
+   * function for uploading image file
+   * @param file
+   * @returns url for image file
+   */
+  public async uploadFile(file: Express.Multer.File) {
+    // throw error for unsupported mimetype
+    if (!['image/jpeg', 'image/png', 'image/jpg'].includes(file.mimetype)) {
+      throw new BadRequestException('mime type not supported');
+    }
+
+    try {
+      // upload the file to the aws s3 bucket and a url has been generated
+      const name = await this.uploadToAwsProvider.fileUpload(file);
+
+      return `${this.configService.get('appConfig.awsCloudFrontUrl')}/${name}`;
+    } catch (error) {
+      throw new ConflictException(error);
+    }
+  }
+}
