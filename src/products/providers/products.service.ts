9999a76ce747e32ece624d886bd047078ea06dd8
ejim11 <favourejim56@gmail.com>
2025-03-26 23:57:37 +0100

    ✅ added the create product functionality to the product service

--- src/products/providers/products.service.ts
+++ src/products/providers/products.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { CreateProductProvider } from './create-product.provider';
+import { CreateProductDto } from '../dtos/createProductDto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    /**
+     * injecting the create product provider
+     */
+
+    private readonly createProductProvider: CreateProductProvider,
+  ) {}
+
+  /**
+   *@function createProduct
+   * @param createProductDto
+   * @param user
+   * @param images
+   * @returns the new product created
+   */
+  public async createProduct(
+    createProductDto: CreateProductDto,
+    files: { [key: string]: Express.Multer.File[] },
+  ) {
+    return this.createProductProvider.createProduct(createProductDto, files);
+  }
+}
