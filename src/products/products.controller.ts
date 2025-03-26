7ebbb4304b51670023f1525ef9604b175c27c079
ejim11 <favourejim56@gmail.com>
2025-03-26 23:58:29 +0100

    ✅ added the create product endpoint to the products controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,16 +1,48 @@
-import { Body, Controller, Post, UploadedFiles } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CreateProductDto } from './dtos/createProductDto';
-import { ActiveUser } from 'src/auth/decorator/active-user.decorator';
-import { ActiveUserData } from 'src/auth/interfaces/active-user-data.interface';
+import { ApiConsumes } from '@nestjs/swagger';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { Roles } from 'src/auth/decorator/role.decorator';
+import { Role } from 'src/auth/enums/role-type.enum';
+import { ProductsService } from './providers/products.service';
 
 @Controller('products')
 export class ProductsController {
-  // create a resource for the productItem
+  constructor(
+    /**
+     * injecting the products service
+     */
+    private readonly productsService: ProductsService,
+  ) {}
 
+  /**
+   * @function createProduct
+   * @param createProductDto
+   * @param user
+   * @param files
+   */
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      // Dynamically generate interceptor fields
+      Array.from({ length: 10 }, (_, i) => ({
+        name: `variants[${i}][images]`,
+        maxCount: 6,
+      })),
+    ),
+  )
+  @Roles(Role.ADMIN)
   @Post()
   public createProduct(
     @Body() createProductDto: CreateProductDto,
-    @ActiveUser() user: ActiveUserData,
-    @UploadedFiles() files: { images?: Express.Multer.File[] },
-  ) {}
+    @UploadedFiles() files: { [key: string]: Express.Multer.File[] },
+  ) {
+    return this.productsService.createProduct(createProductDto, files);
+  }
 }
