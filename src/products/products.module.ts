e00ac2b6c7fc8ab04757807a0eb50fd8588bf769
ejim11 <favourejim56@gmail.com>
2025-03-26 23:59:02 +0100

    ✅ added the create product provider to the products module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,10 +3,12 @@ import { ProductsController } from './products.controller';
 import { ProductsService } from './providers/products.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './product.entity';
+import { CreateProductProvider } from './providers/create-product.provider';
+import { UploadsModule } from 'src/uploads/uploads.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product]), UploadsModule],
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, CreateProductProvider],
 })
 export class ProductsModule {}
