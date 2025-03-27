58fc07a45cdb668034072ae63956c5f294b23c31
ejim11 <favourejim56@gmail.com>
2025-03-27 10:37:13 +0100

    👽️ added the update user route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Patch } from '@nestjs/common';
 
 import { UsersService } from './providers/users.service';
+import { PatchUserDto } from './dtos/patch-user.dto';
+import { Role } from 'src/auth/enums/role-type.enum';
+import { Roles } from 'src/auth/decorator/role.decorator';
+import { ActiveUser } from 'src/auth/decorator/active-user.decorator';
+import { ActiveUserData } from 'src/auth/interfaces/active-user-data.interface';
 
 @Controller('users')
 export class UsersController {
@@ -10,4 +15,13 @@ export class UsersController {
      */
     private readonly usersService: UsersService,
   ) {}
+
+  @Patch()
+  @Roles(Role.USER, Role.ADMIN)
+  public patchUsers(
+    @Body() patchUserDto: PatchUserDto,
+    @ActiveUser() user: ActiveUserData,
+  ) {
+    return this.usersService.updateUser(user.sub, patchUserDto);
+  }
 }
