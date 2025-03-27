4b5d7cf018939daea8fb266dff8b8cc75164e59a
ejim11 <favourejim56@gmail.com>
2025-03-27 10:34:37 +0100

    🏷️ added the patch user dto

---
+++ src/users/dtos/patch-user.dto.ts
@@ -0,0 +1,37 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user.dto';
+import { IsInt, IsOptional, IsString } from 'class-validator';
+
+export class PatchUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
+  @IsString()
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  lasttName?: string;
+
+  @IsOptional()
+  @IsString()
+  country?: string;
+
+  @IsOptional()
+  @IsString()
+  phoneNumber?: string;
+
+  @IsOptional()
+  @IsString()
+  state?: string;
+
+  @IsOptional()
+  @IsString()
+  city?: string;
+
+  @IsOptional()
+  @IsString()
+  streetAddress?: string;
+
+  @IsOptional()
+  @IsInt()
+  zipCode?: number;
+}
