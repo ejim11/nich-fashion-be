f73224a32bd65ba42f5f88787991873570665a61
ejim11 <favourejim56@gmail.com>
2025-03-28 23:59:21 +0100

    🏷️ added the get product dto

---
+++ src/products/dtos/get-product.dto.ts
@@ -0,0 +1,65 @@
+import { IsOptional, IsString } from 'class-validator';
+import { IntersectionType } from '@nestjs/swagger';
+import { PaginationQueryDto } from 'src/common/pagination/dtos/pagination-query.dto';
+
+/**
+ * base dto fot products
+ */
+class GetProductsBaseDto {
+  /**
+   * product name
+   */
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  /**
+   * product category
+   */
+  @IsString()
+  @IsOptional()
+  category?: string;
+
+  /**
+   * productClothType
+   */
+  @IsString()
+  @IsOptional()
+  clothType?: string;
+
+  /**
+   * product price
+   */
+  @IsString()
+  @IsOptional()
+  price?: string;
+
+  /**
+   * product colors
+   */
+  @IsString()
+  @IsOptional()
+  colors?: string;
+
+  /**
+   * product sizes
+   */
+  @IsString()
+  @IsOptional()
+  sizes?: string;
+
+  /**
+   * product dressing style
+   */
+  @IsString()
+  @IsOptional()
+  dressingStyle?: string;
+}
+
+/**
+ * dto for get events
+ */
+export class GetProductsDto extends IntersectionType(
+  GetProductsBaseDto,
+  PaginationQueryDto,
+) {}
