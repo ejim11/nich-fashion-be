2f198b62451d1597ba226e090996f715ed1fcbda
ejim11 <favourejim56@gmail.com>
2025-03-30 10:32:38 +0100

    ♻️ refactored and added the update product function

--- src/products/providers/products.service.ts
+++ src/products/providers/products.service.ts
@@ -5,12 +5,14 @@ import {
 } from '@nestjs/common';
 import { CreateProductProvider } from './create-product.provider';
 import { CreateProductDto } from '../dtos/createProductDto';
-import { Between, In, Like, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Product } from '../product.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { GetProductsDto } from '../dtos/get-product.dto';
 import { Paginated } from 'src/common/pagination/interfaces/paginated.interface';
-import { PaginationProvider } from 'src/common/pagination/providers/pagination.provider';
+import { PatchProductDto } from '../dtos/patch-product.dto';
+import { FindAllProductsProvider } from './find-all-products.provider';
+import { UpdateProductProvider } from './update-product.provider';
 
 @Injectable()
 export class ProductsService {
@@ -28,27 +30,31 @@ export class ProductsService {
     private readonly productRepository: Repository<Product>,
 
     /**
-     * injecting the pagination provider
+     * injecting the find all products provider
      */
-    private readonly paginationprovider: PaginationProvider,
+    private readonly findAllProductsProvider: FindAllProductsProvider,
+
+    /**
+     * injecting the update product provider
+     */
+    private readonly updateProductProvider: UpdateProductProvider,
   ) {}
 
   /**
-   *@function createProduct
+   * @function creates a product
    * @param createProductDto
-   * @param user
-   * @param images
-   * @returns the new product created
+   * @param files
+   * @returns the created product
    */
   public async createProduct(
     createProductDto: CreateProductDto,
     files: { [key: string]: Express.Multer.File[] },
-  ) {
+  ): Promise<Product> {
     return this.createProductProvider.createProduct(createProductDto, files);
   }
 
   /**
-   * function to get product by id
+   * @function to get product by id
    * @param productId
    * @returns product by id
    */
@@ -69,88 +75,26 @@ export class ProductsService {
   }
 
   /**
-   * function for getting all products
+   * @function for getting all products
    * @param productQuery
    * @returns all products
    */
   public async findAll(
     productQuery: GetProductsDto,
   ): Promise<Paginated<Product>> {
-    const { limit, page } = productQuery;
+    return await this.findAllProductsProvider.findAll(productQuery);
+  }
 
-    const whereOptions = [];
-
-    Object.keys(productQuery).forEach((key) => {
-      if (key === 'limit' || key === 'page' || !productQuery[key]) {
-        return;
-      }
-
-      const conditions = {
-        category: productQuery['category']
-          ? In(productQuery['category'].slice().split(','))
-          : undefined,
-        price: productQuery['price']
-          ? Between(
-              parseFloat(productQuery['price'].slice().split('-')[0]),
-              parseFloat(productQuery['price'].slice().split('-')[1]),
-            )
-          : undefined,
-        clothType: productQuery['clothType']
-          ? In(productQuery['clothType'].slice().split(','))
-          : undefined,
-        dressStyle: productQuery['dressStyle']
-          ? In(productQuery['dressStyle'].slice().split(','))
-          : undefined,
-        name: productQuery['name']
-          ? Like(`%${productQuery['name'].slice().split('-').join(' ')}%`)
-          : undefined,
-        variants: {
-          color: productQuery['colors']
-            ? In(productQuery['colors'].slice().split(','))
-            : undefined,
-          size: productQuery['sizes']
-            ? In(productQuery['sizes'].slice().split(','))
-            : undefined,
-        },
-      };
-
-      // Remove undefined nested conditions for variants
-      if (
-        conditions.variants.color === undefined &&
-        conditions.variants.size === undefined
-      ) {
-        conditions.variants = undefined;
-      }
-
-      // Remove undefined conditions
-      const filteredConditions = Object.fromEntries(
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        Object.entries(conditions).filter(([_, v]) => v !== undefined),
-      );
-
-      whereOptions.push(filteredConditions);
-    });
-
-    const checkWhereOptions = Object.keys(whereOptions).length;
-
-    const options = {
-      where: checkWhereOptions ? whereOptions : null,
-      relations: ['variants', 'variants.images'],
-    };
-
-    try {
-      const products = await this.paginationprovider.paginationQuery(
-        {
-          limit: limit,
-          page: page,
-        },
-        this.productRepository,
-        options,
-      );
-
-      return products;
-    } catch (error) {
-      throw new NotFoundException(error);
-    }
+  /**
+   * @function updates a product
+   * @param id
+   * @param patchProductDto
+   * @returns updated product
+   */
+  public async updateProduct(
+    id: string,
+    patchProductDto: PatchProductDto,
+  ): Promise<Product> {
+    return await this.updateProductProvider.updateProduct(id, patchProductDto);
   }
 }
