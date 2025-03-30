725e8de4dac2d7edcd3f080449ff453414961feb
ejim11 <favourejim56@gmail.com>
2025-03-30 15:27:59 +0100

    ✅ added reviews service

--- src/reviews/providers/reviews.service.ts
+++ src/reviews/providers/reviews.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReviewProvider } from './create-review.provider';
+import { CreateReviewDto } from '../dtos/create-review.dto';
 
 @Injectable()
-export class ReviewsService {}
+export class ReviewsService {
+  constructor(
+    /**
+     * injecting the create review provider
+     */
+    private readonly createReviewProvider: CreateReviewProvider,
+  ) {}
+
+  /**
+   * @function creates a review
+   * @param createReviewDto
+   * @param productId
+   * @returns a review
+   */
+  public async createReview(createReviewDto: CreateReviewDto): Promise<{
+    id: string;
+    reviewer: string;
+    stars: number;
+    review: string;
+    dateCreated: Date;
+    product: string;
+  }> {
+    return this.createReviewProvider.createReview(createReviewDto);
+  }
+}
