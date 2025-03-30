8988839139a9f575f5ec091c6511be0344ed45d9
ejim11 <favourejim56@gmail.com>
2025-03-30 10:23:57 +0100

    ✅ added the save variant and find variant by id function

--- src/product-variants/providers/product-variants.service.ts
+++ src/product-variants/providers/product-variants.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { ProductVariant } from '../product-variants.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class ProductVariantsService {}
+export class ProductVariantsService {
+  constructor(
+    /**
+     * injecting the product variant repository
+     */
+    @InjectRepository(ProductVariant)
+    private readonly productVariantRepository: Repository<ProductVariant>,
+  ) {}
+
+  public async findProductVariantById(
+    variantId: string,
+    productId: string,
+  ): Promise<ProductVariant> {
+    const variant = await this.productVariantRepository.findOne({
+      where: { id: variantId, product: { id: productId } },
+    });
+
+    if (!variant) {
+      throw new NotFoundException(
+        `Variant with ID ${variantId} not found for product ${productId}`,
+      );
+    }
+
+    return variant;
+  }
+
+  public async saveProductVariant(
+    variant: ProductVariant,
+  ): Promise<ProductVariant> {
+    return await this.productVariantRepository.save(variant);
+  }
+}
