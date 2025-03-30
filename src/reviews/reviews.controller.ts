3abae560eec38104161542f71a317f64f3a7e02f
ejim11 <favourejim56@gmail.com>
2025-03-30 16:31:46 +0100

    👽️ added the delete reviews api

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { ReviewsService } from './providers/reviews.service';
 import { CreateReviewDto } from './dtos/create-review.dto';
 import { Auth } from 'src/auth/decorator/auth.decorator';
 import { AuthType } from 'src/auth/enums/auth-type.enum';
+import { Roles } from 'src/auth/decorator/role.decorator';
+import { Role } from 'src/auth/enums/role-type.enum';
 
 @Controller('reviews')
 export class ReviewsController {
@@ -29,5 +39,19 @@ export class ReviewsController {
     product: string;
   }> {
     return this.reviewsService.createReview(createReviewDto);
+  }
+
+  /**
+   * @function deletes a product
+   * @param productId
+   * @returns a message that the item was successfully deleted
+   */
+  @Delete('/:reviewId')
+  @Roles(Role.ADMIN)
+  @HttpCode(HttpStatus.NO_CONTENT) // This set
+  public deleteProduct(
+    @Param('reviewId') reviewId: string,
+  ): Promise<{ message: string }> {
+    return this.reviewsService.deleteReview(reviewId);
   }
 }
