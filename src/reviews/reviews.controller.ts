2cd9d680e33011a852d8dfc73c93b44cad7a9264
ejim11 <favourejim56@gmail.com>
2025-03-30 15:28:39 +0100

    👽️ added create review api

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ReviewsService } from './providers/reviews.service';
+import { CreateReviewDto } from './dtos/create-review.dto';
+import { Auth } from 'src/auth/decorator/auth.decorator';
+import { AuthType } from 'src/auth/enums/auth-type.enum';
 
 @Controller('reviews')
-export class ReviewsController {}
+export class ReviewsController {
+  constructor(
+    /**
+     * injecting the reviews service
+     */
+    private readonly reviewsService: ReviewsService,
+  ) {}
+
+  /**
+   * @function creates a review
+   * @param createReviewDto
+   * @returns review
+   */
+  @Post('')
+  @Auth(AuthType.None)
+  public createReview(@Body() createReviewDto: CreateReviewDto): Promise<{
+    id: string;
+    reviewer: string;
+    stars: number;
+    review: string;
+    dateCreated: Date;
+    product: string;
+  }> {
+    return this.reviewsService.createReview(createReviewDto);
+  }
+}
