2611e2e19ffdb043abc46fa3b6cfa3f6746551df
ejim11 <favourejim56@gmail.com>
2025-03-31 23:51:25 +0100

    🏷️ initiate payment dto

---
+++ src/paystack/dtos/initiate-payment.dto.ts
@@ -0,0 +1,49 @@
+// i need info on the product, its variants, delivery info, discount info
+
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { Discount } from 'src/discounts/discounts.entity';
+import { PatchProductVariantDto } from 'src/product-variants/dto/patch-product-variant.dto';
+
+class ProductWithVariantDto {
+  @IsString()
+  @IsNotEmpty()
+  productId: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  price: number;
+
+  @IsArray()
+  @IsNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => PatchProductVariantDto)
+  variants: PatchProductVariantDto[];
+}
+
+export class InitiatePaymentDto {
+  @IsString()
+  @IsOptional()
+  deliveryPicker?: string;
+
+  @IsString()
+  @IsOptional()
+  deliveryAddress?: string;
+
+  @IsString()
+  @IsOptional()
+  discount?: Discount;
+
+  @IsArray()
+  @IsNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => ProductWithVariantDto)
+  products: ProductWithVariantDto[];
+}
