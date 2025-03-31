44b367eb277ce0fe49455c86470839e126c033fa
ejim11 <favourejim56@gmail.com>
2025-03-31 23:52:00 +0100

    ✅ paystack module

---
+++ src/paystack/paystack.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PaystackController } from './paystack.controller';
+import { PaystackService } from './providers/paystack.service';
+import { InitiatePaymentProvider } from './providers/initiate-payment.provider';
+import paystackConfig from './config/paystack.config';
+import { UsersModule } from 'src/users/users.module';
+import { ProductsModule } from 'src/products/products.module';
+import { ProductVariantsModule } from 'src/product-variants/product-variants.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    ProductsModule,
+    ProductVariantsModule,
+    ConfigModule.forFeature(paystackConfig),
+  ],
+  controllers: [PaystackController],
+  providers: [PaystackService, InitiatePaymentProvider],
+})
+export class PaystackModule {}
