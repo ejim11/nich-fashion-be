44362819ef986dca520ac4d7d0a90eea677796bf
ejim11 <favourejim56@gmail.com>
2025-04-02 23:55:46 +0100

    ✅ added the paystack provider

--- src/paystack/paystack.controller.ts
+++ src/paystack/paystack.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Headers,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { PaystackService } from './providers/paystack.service';
 import { Roles } from 'src/auth/decorator/role.decorator';
 import { Role } from 'src/auth/enums/role-type.enum';
 import { InitiatePaymentDto } from './dtos/initiate-payment.dto';
 import { ActiveUser } from 'src/auth/decorator/active-user.decorator';
 import { ActiveUserData } from 'src/auth/interfaces/active-user-data.interface';
+import { Auth } from 'src/auth/decorator/auth.decorator';
+import { AuthType } from 'src/auth/enums/auth-type.enum';
 
 @Controller('paystack')
 export class PaystackController {
@@ -23,5 +33,16 @@ export class PaystackController {
     @ActiveUser() user: ActiveUserData,
   ) {
     return this.paystackService.initiatePayment(initiatePaymentDto, user);
+  }
+
+  @Post('/webhook')
+  @Auth(AuthType.None)
+  @HttpCode(HttpStatus.OK)
+  async handlePaymentWebhook(
+    @Body() body: any,
+    @Headers('x-paystack-signature') signature: string,
+    @Req() req: any,
+  ) {
+    return this.paystackService.verifyPayment(body, signature, req);
   }
 }
