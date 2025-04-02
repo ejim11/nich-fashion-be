4d7ef06006258b4ed7c67771296817be21ed86c2
ejim11 <favourejim56@gmail.com>
2025-04-02 23:53:23 +0100

    ✅ added the payment module

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './payment.entity';
+import { PaymentController } from './payment.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Payment])],
+  controllers: [PaymentController],
 })
 export class PaymentModule {}
