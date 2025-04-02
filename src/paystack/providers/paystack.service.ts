8b313244ff9526a7dcbc5ef40369ddbbf1cf9cdb
ejim11 <favourejim56@gmail.com>
2025-04-02 23:55:06 +0100

    ✅ added verify payment and webhook functions to the paystack service

--- src/paystack/providers/paystack.service.ts
+++ src/paystack/providers/paystack.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InitiatePaymentProvider } from './initiate-payment.provider';
 import { InitiatePaymentDto } from '../dtos/initiate-payment.dto';
 import { ActiveUserData } from 'src/auth/interfaces/active-user-data.interface';
+import { VerifyPaymentProvider } from './verify-payment.provider';
 
 @Injectable()
 export class PaystackService {
@@ -10,6 +11,11 @@ export class PaystackService {
      * injecting the initiate payment provider
      */
     private readonly initiatePaymentProvider: InitiatePaymentProvider,
+
+    /**
+     * injecting the verify payment provider
+     */
+    private readonly verifyPaymentProvider: VerifyPaymentProvider,
   ) {}
 
   public async initiatePayment(
@@ -19,6 +25,14 @@ export class PaystackService {
     return await this.initiatePaymentProvider.initializePayment(
       initiatePaymentDto,
       user,
+    );
+  }
+
+  public async verifyPayment(body: any, signature: string, req: any) {
+    return await this.verifyPaymentProvider.paymentWebhook(
+      body,
+      signature,
+      req,
     );
   }
 }
