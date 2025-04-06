60504191e505c3d87726c168cc4633fc2545583e
ejim11 <favourejim56@gmail.com>
2025-04-07 00:00:37 +0100

    ✅ updated the module with the controller

--- src/product-variants/product-variants.module.ts
+++ src/product-variants/product-variants.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ProductVariantsService } from './providers/product-variants.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductVariant } from './product-variants.entity';
+import { ProductVariantsController } from './product-variants.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ProductVariant])],
   providers: [ProductVariantsService],
   exports: [ProductVariantsService],
+  controllers: [ProductVariantsController],
 })
 export class ProductVariantsModule {}
