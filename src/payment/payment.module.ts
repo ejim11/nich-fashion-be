ccd9a99ae0644ae294824808968c748c038b9c1d
ejim11 <favourejim56@gmail.com>
2025-04-06 23:56:22 +0100

    ✅ added the payment variant module

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './payment.entity';
 import { PaymentController } from './payment.controller';
+import { PaymentVariant } from './payment-variant.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [TypeOrmModule.forFeature([Payment, PaymentVariant])],
   controllers: [PaymentController],
 })
 export class PaymentModule {}
