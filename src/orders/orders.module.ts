69b34ef64b8a6cbc2af86332e2621da10f65a436
ejim11 <favourejim56@gmail.com>
2025-04-07 21:24:10 +0100

    ✅ updated the module with deps

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,10 +3,12 @@ import { OrdersController } from './orders.controller';
 import { OrdersService } from './providers/orders.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './order.entity';
+import { FindAllOrdersProvider } from './providers/find-all-orders.provider';
+import { PaginationModule } from 'src/common/pagination/pagination.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
+  imports: [PaginationModule, TypeOrmModule.forFeature([Order])],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, FindAllOrdersProvider],
 })
 export class OrdersModule {}
