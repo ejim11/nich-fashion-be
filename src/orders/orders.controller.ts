fb229c0ab94a581bfb3a1034ac034ee7b750fe92
ejim11 <favourejim56@gmail.com>
2025-04-07 21:23:57 +0100

    👽️ added route for updating orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
+import { PatchOrderDto } from './dtos/patch-order.dto';
+import { Roles } from 'src/auth/decorator/role.decorator';
+import { Role } from 'src/auth/enums/role-type.enum';
+import { OrdersService } from './providers/orders.service';
+import { GetOrdersDto } from './dtos/get-orders.dto';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  constructor(
+    /**
+     * injecting the orders service
+     */
+    private readonly ordersService: OrdersService,
+  ) {}
+
+  @Patch('/:orderId')
+  @Roles(Role.ADMIN)
+  public updateOrder(
+    @Body() patchOrderDto: PatchOrderDto,
+    @Param('orderId') orderId: string,
+  ) {
+    return this.ordersService.updateOrder(patchOrderDto, orderId);
+  }
+
+  @Get('/:orderId')
+  public findOrderById(@Param('orderId') orderId: string) {
+    return this.ordersService.findOrderById(orderId);
+  }
+
+  @Get('')
+  public findAllOrders(@Query() orderQuery: GetOrdersDto) {
+    return this.ordersService.findAll(orderQuery);
+  }
+}
